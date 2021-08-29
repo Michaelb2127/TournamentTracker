d1c584d8881094dba4e8ba4a2040aedf97b99db8
mbell8464 <michaelb2127@charter.net>
2021-08-28 21:05:37 -0500

    Built out TrackerLibrary with all models with properties

---
+++ TournamentTracker/TrackerLibrary/TeamModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public class TeamModel
+    {
+        //props
+        public List<PersonModel> TeamMembers { get; set; } = new List<PersonModel>(); //Initialize the List of Person (versus making a ctor)
+        public string TeamName { get; set; }
+    }
+}
